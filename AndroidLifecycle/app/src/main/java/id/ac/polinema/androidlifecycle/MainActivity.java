0afca9d71da2585f0738cc724db20b96ca239600
rizkinovitria139 <57834522+rizkinovitria139@users.noreply.github.com>
2020-02-13 10:31:49 +0700

    awal

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -12,35 +12,7 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	protected void onStart(){
-		super.onStart();
-		android.widget.Toast.makeText(this, "App on Start", Toast.LENGTH_SHORT).show();
-	}
 
-	@Override
-	protected void onStop(){
-		super.onStop();
-		android.widget.Toast.makeText(this, "App on Stop", Toast.LENGTH_SHORT).show();
-	}
-
-	@Override
-	protected  void onRestart(){
-		super.onRestart();
-		android.widget.Toast.makeText(this, "Application on Restart", Toast.LENGTH_SHORT).show();
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		Toast.makeText(this, "Application On Pause", Toast.LENGTH_SHORT).show();
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		Toast.makeText(this, "Application On Destroy", Toast.LENGTH_SHORT).show();
-	}
 
 
 	// TODO: tambahkan callback onStart() di sini
